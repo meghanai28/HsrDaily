3c87526cd0c0f83c5d85137bbfd57e341575022e
Meghana Indukuri <66529359+meghanai28@users.noreply.github.com>
2026-03-12 18:33:06 -0700

    auto-daily check in completed

--- module2/hoyolab_api.py
+++ module2/hoyolab_api.py
@@ -3,19 +3,53 @@ import json
 
 # GET REQUEST
 
-url = 'https://sg-public-api.hoyolab.com/event/luna/os/info?act_id=e202303301540311&lang=en-us'
+INFO_URL = 'https://sg-public-api.hoyolab.com/event/luna/os/info?act_id=e202303301540311&lang=en-us'
+CHECK_IN_URL = 'https://sg-public-api.hoyolab.com/event/luna/os/sign'
 
-with open("../config.json") as f:
-    config = json.load(f)
+class ApiService:
+    def __init__(self):
+    
+        with open("../config.json") as f:
+            self.config = json.load(f)
+        self.cookie = self.config["hoyolab"]["cookie"]
 
-cookie = config["hoyolab"]["cookie"]
 
-response = requests.get(url, headers={"Cookie": cookie})
+    def get_response(self):
+        response = requests.get(INFO_URL, headers={"Cookie": self.cookie})
+        print(f"Status Code: {response.status_code}")
+        print(f"Response Text: {response.text}")
+        data = None
+        if response.status_code == 200 and 'json' in response.headers.get('Content-Type',''):
+            data = response.json()
+            print(f"parsed JSON keys: {data.keys()}")
+        return data
 
-print(f"Status Code: {response.status_code}")
+    def check_is_sign(self):
+        data = self.get_response()
+        if data:
+            print(data['data']['is_sign'])
+            return data['data']['is_sign']
+        return False
+    
+    def do_check_in(self):
+        if not self.check_is_sign():
+            response = requests.post(CHECK_IN_URL, headers={"Cookie": self.cookie}, json={"act_id": "e202303301540311", "lang": "en-us"})
+            data = response.json()
+            if data["retcode"] == 0:
+                return True
+            else:
+                print(f"Error posting to check in")
+                return False
+        else:
+            print("Already Checked In")
+            return False
 
-print(f"Response Text: {response.text}")
+def main():
+    api_service = ApiService()
+    api_service.do_check_in()
 
-if response.status_code == 200 and 'json' in response.headers.get('Content-Type',''):
-    data = response.json()
-    print(f"parsed JSON keys: {data.keys()}")
+if __name__ == "__main__":
+    main()
+
+        
+
